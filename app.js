28aac2cbaaaec6ca5afb5b319a6ad244b0bca2b9
remy <remy.chaumard@ubisoft.com>
2016-05-27 17:47:20 -0400

    trying to join a server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.js')
 var bot = new Discord.Client()
-var token = require('./token')
+var settings = require('./settings')
 
 bot.on('ready', function () {
 	console.log(bot.username, '-', bot.id)
@@ -11,10 +11,18 @@ bot.on('message', function (user, userID, chan, message, event) {
 	bot.sendMessage({to: chan, message: 'lol'})
 })
 
-console.log('using token...', token);
-bot.loginWithToken(token)
+bot.loginWithToken(settings.token)
 .then(function (token) {
-	console.log('logged in !', token)
+	console.log('logged in !', token);
+	console.log('joining...', settings.invite);
+
+	bot.joinServer(settings.invite)
+	.then(function () {
+		console.log('joined')
+	}, function () {
+		console.error('could not join')
+	})
+
 
 }, function couldNotLogin (err) {
 	console.log(err);
