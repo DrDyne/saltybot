be0e13783e6709ff14a4c6bae58758fe1065a154
remy chaumard <r.chaumard@gmail.com>
2016-10-15 19:04:19 -0400

    upgraded error and startup messages with memes

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ client.on('message', (message) => {
   console.log('?', message.content)
   bot.read(message)
      .then(bot.reply(message))
-     .catch(console.error)
+     .catch(console.error.bind(null, '(╯°□°)╯︵ ┻━┻'))
 })
 
 console.log('connecting...')
 client.login(settings.token);
 
 client.on('ready', () => {
-  console.log('connected')
+  console.log('t(ಠ_ಠt) sup\' bb?')
 })
