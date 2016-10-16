bc96bdb0a14974d67b95d8621dddeaa25e011f9b
remy chaumard <r.chaumard@gmail.com>
2016-10-16 01:10:34 -0400

    bot is now instancied after login and receives a user

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const Discord = require('discord.js')
 const Bot = require('./bot')
 const client = new Discord.Client()
 
-var bot = new Bot(client)
-
-client.on('message', (message) => {
-  console.log('?', message.content)
-  bot.read(message)
-     .then(bot.reply(message))
-     .catch(console.error.bind(null, '(╯°□°)╯︵ ┻━┻'))
-})
-
 console.log('connecting...')
 client.login(settings.token);
 
 client.on('ready', () => {
-  console.log('t(ಠ_ಠt) sup\' bb?')
+  var bot = new Bot(client, settings)
+  console.log('(ಠ_ಠ)/ sup\' bb?')
+  console.log('- %s', bot.name)
+
+  client.on('message', (message) => {
+    console.log('?', message.content)
+    bot.read(message)
+       .then(bot.reply(message))
+       .catch(console.error.bind(null, '(╯°□°)╯︵ ┻━┻'))
+  })
+
 })
